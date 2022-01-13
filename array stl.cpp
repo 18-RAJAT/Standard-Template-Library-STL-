02f1762334a7e2b7fad7cc23892089674ddb015d
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-14 00:19:50 +0530

    Add files via upload

---
+++ array stl.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //declare
+
+    std::array<int,5>myarray;
+
+    //initialization
+    
+    std::array<int,5>myarray={1,2,3,4,5};//initializer list
+
+    std::array<int,5>myarray={1,2,3,4,5};//uniform initializer
+
+    //assign using initializer list
+
+    return 0;
+}
---
+++ auto keyword.cpp
@@ -0,0 +1,34 @@
+/*
+  auto keyword and rangebase loop ko combine krke code is readable and good practice
+
+  and small code....!!
+  
+*/
+
+#include<bits/stdc++.h>
+using namespace  std;
+
+int main()
+{
+
+    //auto is a keyword,this is already assigned the value automaticly...!! 
+    auto a = (1,2,3,4,5,6);
+    auto b=(1,2,3);
+
+    auto c=1;
+
+    /* auto is dynamically a ka data type define kr deta hai
+       
+       in simple words that was assuming the value by user
+
+       is gagve them...!! 
+    */
+     cout<<a<<endl;
+     cout<<b<<endl;
+}
+
+
+//last value is assign...if 1 and more values are avaliable in the set and other thing
+
+
+//auto is assuming a PAIR and a VECTOR.....!!
